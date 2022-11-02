6569dd13878e03cbbd13d16ed21ef3da79f34f5d
Alexander <alex17.05.84@yandex.ru>
2022-11-02 18:30:46 +0300

    Task 03 done

---
+++ Exercise_03/Program03.cs
@@ -0,0 +1,26 @@
+﻿int GetNumber()
+{
+    // do
+    // {
+    //     text = Console.ReadLine();
+    // }
+    // while (text.IndexOf(',') >= 0 || text.IndexOf('.') >= 0);
+    string text = "";
+    bool key = true;
+    while (key)
+    {
+        text = Console.ReadLine();
+        if (text.IndexOf(',') >= 0 || text.IndexOf('.') >= 0)
+        {
+            Console.WriteLine("Введите верное число!!!");
+            key = true;
+        }
+        else
+        {
+            key = false;
+        }
+    
+    }
+    return Convert.ToInt32(text);
+}
+    Console.WriteLine(GetNumber());
