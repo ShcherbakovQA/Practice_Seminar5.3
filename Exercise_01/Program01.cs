77910f181f15ed30c4595de88e410ea22e08ac87
Alexander <alex17.05.84@yandex.ru>
2022-11-02 13:49:59 +0300

    Task 01

--- Exercise_01/Program01.cs
+++ Exercise_01/Program01.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int a=5
+// int b=2
+int GetMulti(int a, int b)
+{
+    return a*b;
+}
+
+//***********************************
+// ОСНОВНАЯ ПРОГРАММА 
+
+// int number1 = new Random().Next(0,100); 
+// int number2 = new Random().Next(0,100); 
+
+int number1 = 5;
+int number2 = 2;
+
+Console.WriteLine(GetMulti(number1,number2));
+
+
